645d139f96cd550eed73718edb75233c4b169175
Phu-Quy Felicia Trinh <35504528+feliciatrinh@users.noreply.github.com>
2019-04-19 01:26:33 -0700

    Update tests.py

--- menuapi/menu/tests.py
+++ menuapi/menu/tests.py
@@ -1,8 +1,4 @@
-import json
 from django.test import TestCase
-from django.urls import reverse
-from rest_framework import status
-from rest_framework.test import APITestCase
 
 from .models import MenuSection
 from .serializers import MenuSectionSerializer
